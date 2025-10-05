e218e8ffd5476b1fdcc81efc5fd3c6dc0302e88a
pero4l <lilpero03@gmail.com>
2025-10-05 22:17:17 +0100

    ensure my drop down is working well

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="">
       <HeroPage/>
       <FeaturePage/>
-      <PricingPage/>
+      {/* <PricingPage/> */}
       <TestimonialPage/>
       <CtaPage/>
       
