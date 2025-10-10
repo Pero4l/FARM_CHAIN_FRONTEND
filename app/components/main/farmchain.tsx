c77a9f756eab3c59e30d1381a608d69fe2e6f603
pero4l <lilpero03@gmail.com>
2025-10-10 23:46:46 +0100

    changed the bg to green 100

--- app/components/main/farmchain.tsx
+++ app/components/main/farmchain.tsx
@@ -602,8 +602,8 @@ const FarmChain: React.FC = () => {
           {mainMenu ? <X/> : <Menu/>}
           </div>
 
-              <div className="lg:hidden flex items-center bg-gray-100 rounded-2xl px-4 py-2.5 w-96">
-                <Search className="w-5 h-5 text-gray-400 mr-3" />
+              <div className="lg:hidden flex items-center bg-green-100 rounded-2xl px-4 py-2.5 w-96">
+                <Search className="w-5 h-5 text-gray-500 mr-3" />
                 <input
                   type="text"
                   placeholder="Search farmers, products, insights..."
