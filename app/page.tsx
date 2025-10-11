107ad3b872d72966366268cd8eac8cc1607945e5
olawale3334 <olawale3334@gmail.com>
2025-10-11 09:40:25 +0100

    merge

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,6 @@ import FeaturePage from "./components/features/features";
 import PricingPage from "./components/pricing/pricing";
 import TestimonialPage from "./components/testimonial/testimonial";
 import CtaPage from "./components/cta-section/cta";
-import MainNavPage from "./components/main/navbar/page";
-import FarmChain from "./components/main/farmchain";
-
-
-// interface MainNavPageProps {
-//   setMenu: React.Dispatch<React.SetStateAction<boolean>>;
-// }
-
 export default function Home() {
 
 
@@ -26,9 +18,6 @@ export default function Home() {
       <TestimonialPage/>
       <CtaPage/>
       
-
-      {/* <MainNavPage />
-      <FarmChain /> */}
     </div>
   );
 }
