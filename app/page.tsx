91a5e7a3b941fcadaf188e9506ddf14d9d6e0dcc
pero4l <lilpero03@gmail.com>
2025-10-11 22:59:08 +0100

    uncommented my main to see what i am working on

--- app/page.tsx
+++ app/page.tsx
@@ -18,18 +18,16 @@ export default function Home() {
   return (
 
     <div className="">
-      <HeroPage/>
-      <FeaturePage/>
+      {/* <HeroPage/>
+      <FeaturePage/> */}
 
       {/* <PricingPage/> */}
       
-      <TestimonialPage/>
-      <CtaPage/>
-       
+    /
       
 
-      {/* <MainNavPage />
-      <FarmChain /> */}
+      <MainNavPage />
+      <FarmChain />
     </div>
   );
 }
