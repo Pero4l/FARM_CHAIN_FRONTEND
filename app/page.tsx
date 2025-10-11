7fdab17c99ff405ec6710cfb9eb3a28da15a4c32
pero4l <lilpero03@gmail.com>
2025-10-11 02:28:21 +0100

    commented my main since i am not working on it for now and brought back my landing page

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ export default function Home() {
   return (
 
     <div className="">
-      {/* <HeroPage/>
-      <FeaturePage/> */}
+      <HeroPage/>
+      <FeaturePage/>
 
       {/* <PricingPage/> */}
       
-      {/* <TestimonialPage/>
-      <CtaPage/> */}
+      <TestimonialPage/>
+      <CtaPage/>
       
 
-      <MainNavPage />
-      <FarmChain />
+      {/* <MainNavPage />
+      <FarmChain /> */}
     </div>
   );
 }
