251c9f5689b3fbbcf470987a0726c1b42c4af359
pero4l <lilpero03@gmail.com>
2025-10-12 22:56:03 +0100

    uncommented my landing page back

--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ export default function Home() {
   return (
 
     <div className="">
-      {/* <HeroPage/>
-      <FeaturePage/> */}
+      <HeroPage/>
+      <FeaturePage/>
 
       {/* <PricingPage/> */}
       
-      {/* <TestimonialPage/>
+      <TestimonialPage/>
       <CtaPage/>
-       */}
+      
 
-      <MainNavPage />
-      <FarmChain />
+      {/* <MainNavPage />
+      <FarmChain /> */}
     </div>
   );
 }
